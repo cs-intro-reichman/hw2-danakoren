cc4fe074643b43f8b896bbaec61c5cc01cfeb02e
danakoren <dana.koren@post.runi.ac.il>
2024-11-19 22:09:29 +0200

    try 4

--- Cheers.java
+++ Cheers.java
@@ -2,5 +2,23 @@
 public class Cheers {
         public static void main(String[] args) {
 	    // Replace this comment with your code
+            int numTimes = Integer.parseInt(args[0]);
+            String s = (args[1]).toUpperCase();
+            //String s0out = " ";
+            String letters = "AEFHILMNORSX";
+            for(int i = 0 ; i < s.length(); i ++){
+                if(letters.indexOf(s.charAt(i)) == -1){
+                        System.out.println("Give me a  " + s.charAt(i) + ": " + s.charAt(i) + "!");
+                }else{
+                        System.out.println("Give me an " + s.charAt(i) + ": " + s.charAt(i) + "!");
+   
+                }
+                
+            }
+            System.out.println("What does that spell?");
+                for(int j = 0 ; j < numTimes; j ++){
+                        System.out.println(s + " !!!");
+
+                }
         }
 }
