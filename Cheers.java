a13ec9c148fc9ae8d9aa4106935b5ec3ae18de34
danakoren <dana.koren@post.runi.ac.il>
2024-11-19 22:24:44 +0200

    try 6

--- Cheers.java
+++ Cheers.java
@@ -1,10 +1,8 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
-            String s = (args[1]).toUpperCase();
-            int numTimes = Integer.parseInt(args[0]);
-            //String s0out = " ";
+            String s = (args[0]).toUpperCase();
+            int numTimes = Integer.parseInt(args[1]);
             String letters = "AEFHILMNORSX";
             for(int i = 0 ; i < s.length(); i ++){
                 if(letters.indexOf(s.charAt(i)) == -1){
@@ -12,8 +10,7 @@ public class Cheers {
                 }else{
                         System.out.println("Give me an " + s.charAt(i) + ": " + s.charAt(i) + "!");
    
-                }
-                
+                } 
             }
             System.out.println("What does that spell?");
                 for(int j = 0 ; j < numTimes; j ++){
