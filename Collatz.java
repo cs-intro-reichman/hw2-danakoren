b923688217a2cadc8222158ac79c1b3cb09d129b
danakoren <dana.koren@post.runi.ac.il>
2024-11-21 20:42:28 +0200

    Collatz try3

--- Collatz.java
+++ Collatz.java
@@ -11,25 +11,25 @@ public class Collatz {
 			for (int seed = 1 ; seed <= N ; seed ++){
 			int sum = 0;
 			int newSeed = seed;
-			do{
+			do {
 				System.out.print(newSeed + " ");
-				if (newSeed % 2 == 0){
+				if (newSeed % 2 == 0) {
 					newSeed /= 2;
 				} else {
 					newSeed = (newSeed * 3) + 1;
 				}
 				sum++;
-			}while(newSeed!=1);
+			} while (newSeed != 1);
+			System.out.print(newSeed + " ");
 			System.out.println( "(" + (sum + 1) + ")" );
 			
 		}
 		System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
 	}
-	else {
+	else 
 		if ( mode.charAt(0) == c.charAt(0)){
 			System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
 		}
 	}
 	
-}
 }
